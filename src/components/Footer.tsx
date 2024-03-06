4fac4eef2142a8216115ec58e4ca5d9a8a59a9d2
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-06 10:53:14 -0400

    Traduciendo la descripción de About Me

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,13 @@
+import { useTranslation } from "react-i18next";
+
 const Footer = () => {
+  const [t] = useTranslation("global");
+
   return (
     <footer className="footer">
       <p className="footer__alias">Pancratzia</p>
       <p className="footer__text">
-        © {new Date().getFullYear()} Laura Ortega. All rights reserved.
+        © {new Date().getFullYear()} Laura Ortega. {t("footer")}.
       </p>
     </footer>
   );
