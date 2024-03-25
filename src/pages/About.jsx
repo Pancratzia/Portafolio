07c0e93404cc296114edd0277c6e8c442da63d95
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-25 15:02:28 -0400

    Agregando texto para el About Me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,30 +13,25 @@ const About = () => {
     >
       <Header title={"< About Me />"} pageName={"About Me"} />
 
-      <motion.div
-        className="w-64 h-64 my-5 mx-auto"
-        initial={{ rotate: 45, scale: 0.5, opacity: 0 }}
-        animate={{ rotate: 0, scale: 1, opacity: 1 }}
-        exit={{ rotate: 45, scale: 0.5, opacity: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <RoundedPicture
-          imageUrl="./img/personal/profile"
-          description="Laura Ortega - FullStack Web Developer Picture"
-        />
-      </motion.div>
+      <div className="container my-0 mx-auto grid md:grid-cols-[repeat(1,1fr)] lg:grid-cols-[1fr_2fr] items-center gap-2 lg:gap-10">
+        <motion.div
+          className="w-64 h-64 my-5 mx-auto lg:w-96 lg:h-96"
+          initial={{ rotate: 45, scale: 0.5, opacity: 0 }}
+          animate={{ rotate: 0, scale: 1, opacity: 1 }}
+          exit={{ rotate: 45, scale: 0.5, opacity: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <RoundedPicture
+            imageUrl="./img/personal/profile"
+            description="Laura Ortega - FullStack Web Developer Picture"
+          />
+        </motion.div>
 
-      <div className="">
-        <p className="text-center text-white">
-          Como DESARROLLADORA FULLSTACK, me impulsa una pasión inquebrantable
-          por la PROGRAMACIÓN, un amor por la RESOLUCIÓN DE PROBLEMAS, y una
-          curiosidad insaciable por la exploración. Vivo en la búsqueda
-          constante de conocimiento, fijando un compromiso con el aprendizaje
-          continuo. Gracias a mis estudios en INGENIERÍA TELEMÁTICA en la
-          Universidad Centro Occidental Lisandro Alvarado en Barquisimeto,
-          Venezuela, he llegado a entender que ser programador es un VIAJE DE
-          APRENDIZAJE Y DESAPRENDIZAJE PERPETUO.
-        </p>
+        <div className="border-2 border-yellow-500 p-5 rounded-xl text-indigo-100 ">
+          <p className="text-center text-white fluid-md">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi libero quis explicabo possimus debitis, illo quo dignissimos perspiciatis sapiente cupiditate quos quibusdam, inventore voluptate quas officiis cum molestias atque vero eos. Vel, dolores saepe ipsum quisquam laudantium at veniam reiciendis minima optio distinctio culpa pariatur dolorum quae soluta. Illo quos veritatis cumque asperiores, ut dolores dolor? Tempora blanditiis debitis consequuntur quis a officia unde aspernatur odit ad animi, necessitatibus doloribus.
+          </p>
+        </div>
       </div>
     </motion.div>
   );
