c8db953f7bcdfd7baf62a337c3834eacdfc93018
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-25 11:28:43 -0400

    Agregando Animaciones de Framer Motion

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,15 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 
+import { AnimatePresence } from "framer-motion";
+
 const AppRoutes = () => {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<div>About Me</div>} />
-      <Route path="/skills" element={<div>Skills</div>} />
-      <Route path="/projects" element={<div>Projects</div>} />
-      <Route path="/contact" element={<div>Contact Me</div>} />
-    </Routes>
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<div>About Me</div>} />
+        <Route path="/skills" element={<div>Skills</div>} />
+        <Route path="/projects" element={<div>Projects</div>} />
+        <Route path="/contact" element={<div>Contact Me</div>} />
+      </Routes>
+    </AnimatePresence>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,19 @@
 import AnimatedText from "../components/utils/AnimatedText";
 import RoundedPicture from "../components/utils/RoundedPicture";
-import {
-  FaSquareXTwitter,
-  FaSquareGithub,
-  FaLinkedin
-} from "react-icons/fa6";
+import { FaSquareXTwitter, FaSquareGithub, FaLinkedin } from "react-icons/fa6";
 import SocialSquare from "../components/utils/SocialSquare";
 import RedirectLinkButton from "../components/utils/RedirectLinkButton";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div className="flex justify-center flex-col min-h-screen py-6">
+    <motion.div
+      className="flex justify-center flex-col min-h-screen py-6"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.5 }}
+      transition={{ duration: 0.5 }}
+    >
       {/* main title */}
       <header className="container my-0 mx-auto mb-6 flex items-center justify-center flex-col">
         <h1 className="fluid-4xl py-1 md:py-5 font-extrabold">
@@ -21,12 +24,18 @@ const Home = () => {
         </h3>
       </header>
 
-      <div className="container my-0 mx-auto w-80 h-80 md:w-96 md:h-96">
+      <motion.div
+        className="container my-0 mx-auto w-80 h-80 md:w-96 md:h-96"
+        initial={{ rotate: 45, scale: 0.5, opacity: 0 }}
+        animate={{ rotate: 0, scale: 1, opacity: 1 }}
+        exit={{ rotate: 45, scale: 0.5, opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
         <RoundedPicture
           imageUrl="./img/personal/profile"
           description="Laura Ortega - FullStack Web Developer Picture"
         />
-      </div>
+      </motion.div>
 
       {/* Socials */}
 
@@ -57,7 +66,7 @@ const Home = () => {
       <div className="flex justify-center mt-5">
         <RedirectLinkButton text={"About Me"} link={"/about"} />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
