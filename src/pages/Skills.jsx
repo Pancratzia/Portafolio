0af1b4bed76efa6841e91e040996134f04494f7f
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-28 14:26:23 -0400

    Estructurando parte de la card para las skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,11 +1,23 @@
-import MainLayout from "../components/Layouts/MainLayout"
+import MainLayout from "../components/Layouts/MainLayout";
+import { DiHtml5 } from "react-icons/di";
 
 const Skills = () => {
   return (
     <MainLayout title={"< Skills />"} pageName={"Skills"}>
-        <h1>Skills</h1>
-    </MainLayout>
-  )
-}
+      <div className="text-slate-100 container grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="">
+          <h3 className="font-bold fluid-lg uppercase text-center mb-5">
+            FrontEnd
+          </h3>
 
-export default Skills
+          <div className="w-20 h-20 bg-slate-950 rounded-full flex justify-center items-center flex-col">
+            <h5 className="sr-only md:not-sr-only fluid-md">HTML</h5>
+            <DiHtml5 className="w-10 h-10" />
+          </div>
+        </div>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default Skills;
