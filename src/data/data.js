8ceacf4f264ca4c5adb3b572cf4a3010eb224e50
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-28 20:26:04 -0400

    Skills finalizados. Falta crear componente

--- src/data/data.js
+++ src/data/data.js
@@ -1,77 +1,291 @@
-import { FaSquareXTwitter, FaSquareGithub, FaLinkedin, FaHouse, FaUser, FaCubesStacked, FaLaptopCode, FaEnvelope, FaMusic, FaBookOpenReader, FaDog, FaGamepad } from "react-icons/fa6";
+import {
+  FaSquareXTwitter,
+  FaSquareGithub,
+  FaLinkedin,
+  FaHouse,
+  FaUser,
+  FaCubesStacked,
+  FaLaptopCode,
+  FaEnvelope,
+  FaMusic,
+  FaBookOpenReader,
+  FaDog,
+  FaGamepad,
+} from "react-icons/fa6";
+import {
+  DiHtml5,
+  DiCss3,
+  DiBootstrap,
+  DiSass,
+  DiReact,
+  DiAngularSimple,
+  DiNodejsSmall,
+  DiJava,
+  DiJavascript,
+  DiPhp,
+  DiGit,
+  DiNpm,
+  DiVisualstudio,
+  DiNetbeans,
+  DiEclipse,
+  DiMysql,
+} from "react-icons/di";
+import {
+  SiTailwindcss,
+  SiVuedotjs,
+  SiExpress,
+  SiSpringboot,
+  SiLaravel,
+  SiCplusplus,
+  SiPnpm,
+  SiYarn,
+  SiNetlify,
+  SiFirebase,
+  SiVercel,
+  SiPostman,
+  SiVite,
+  SiMariadb,
+  SiSqlite,
+  SiPostgresql,
+} from "react-icons/si";
 
 export const socials = [
-    {
-        Icon: FaSquareXTwitter,
-        text: "Twitter X",
-        links: "https://twitter.com/Pancratzia"
-    },
-    {
-        Icon: FaSquareGithub,
-        text: "Github X",
-        links: "https://github.com/Pancratzia"
-    },
-    {
-        Icon: FaLinkedin,
-        text: "Linkedin X",
-        links: "https://www.linkedin.com/in/leortegaperez/"
-    }
-]
+  {
+    Icon: FaSquareXTwitter,
+    text: "Twitter X",
+    links: "https://twitter.com/Pancratzia",
+  },
+  {
+    Icon: FaSquareGithub,
+    text: "Github X",
+    links: "https://github.com/Pancratzia",
+  },
+  {
+    Icon: FaLinkedin,
+    text: "Linkedin X",
+    links: "https://www.linkedin.com/in/leortegaperez/",
+  },
+];
 
 export const routes = [
-    {
-        Icon: FaHouse,
-        text: "Home",
-        link: "/"
-    },
-    {
-        Icon: FaUser,
-        text: "About Me",
-        link: "/about"
-    },
-    {
-        Icon: FaCubesStacked,
-        text: "Skills",
-        link: "/skills"
-    },
-    {
-        Icon: FaLaptopCode,
-        text: "Projects",
-        link: "/projects"
-    },
-    {
-        Icon: FaEnvelope,
-        text: "Contact",
-        link: "/contact"
-    }
-]
+  {
+    Icon: FaHouse,
+    text: "Home",
+    link: "/",
+  },
+  {
+    Icon: FaUser,
+    text: "About Me",
+    link: "/about",
+  },
+  {
+    Icon: FaCubesStacked,
+    text: "Skills",
+    link: "/skills",
+  },
+  {
+    Icon: FaLaptopCode,
+    text: "Projects",
+    link: "/projects",
+  },
+  {
+    Icon: FaEnvelope,
+    text: "Contact",
+    link: "/contact",
+  },
+];
 
 export const interests = [
-    {
-        Icon: FaMusic,
-        text: "Music",
-    },
-    {
-        Icon: FaBookOpenReader,
-        text: "Reading",
-    },
-    {
-        Icon: FaDog,
-        text: "Dogs",
-    },
-    {
-        Icon: FaGamepad,
-        text: "Videogames",
-    }
-]
+  {
+    Icon: FaMusic,
+    text: "Music",
+  },
+  {
+    Icon: FaBookOpenReader,
+    text: "Reading",
+  },
+  {
+    Icon: FaDog,
+    text: "Dogs",
+  },
+  {
+    Icon: FaGamepad,
+    text: "Videogames",
+  },
+];
 
 export const languages = [
-    {
-        text: "Spanish",
-        level: "Native"
-    },
-    {
-        text: "English",
-        level: "Fluent"
-    }
-]
+  {
+    text: "Spanish",
+    level: "Native",
+  },
+  {
+    text: "English",
+    level: "Fluent",
+  },
+];
+
+export const techStak = [
+  {
+    name: "FrontEnd",
+    stack: [
+      {
+        name: "HTML",
+        Icon: DiHtml5,
+      },
+      {
+        name: "CSS",
+        Icon: DiCss3,
+      },
+      {
+        name: "SASS",
+        Icon: DiSass,
+      },
+      {
+        name: "Bootstrap",
+        Icon: DiBootstrap,
+      },
+      {
+        name: "Tailwind",
+        Icon: SiTailwindcss,
+      },
+      {
+        name: "React",
+        Icon: DiReact,
+      },
+      {
+        name: "Angular",
+        Icon: DiAngularSimple,
+      },
+      {
+        name: "VUE",
+        Icon: SiVuedotjs,
+      },
+    ],
+  },
+  {
+    name: "BackEnd",
+    stack: [
+      {
+        name: "Node",
+        Icon: DiNodejsSmall,
+      },
+      {
+        name: "Express",
+        Icon: SiExpress,
+      },
+      {
+        name: "Laravel",
+        Icon: SiLaravel,
+      },
+      {
+        name: "Spring",
+        Icon: SiSpringboot,
+      },
+      {
+        name: "Firebase",
+        Icon: SiFirebase,
+      },
+    ],
+  },
+  {
+    name: "Languages",
+    stack: [
+      {
+        name: "Java",
+        Icon: DiJava,
+      },
+      {
+        name: "C++",
+        Icon: SiCplusplus,
+      },
+      {
+        name: "PHP",
+        Icon: DiPhp,
+      },
+      {
+        name: "Javascript",
+        Icon: DiJavascript,
+      },
+    ],
+  },
+  {
+    name: "DevOps",
+    stack: [
+      {
+        name: "Git",
+        Icon: DiGit,
+      },
+      {
+        name: "GitHub",
+        Icon: FaSquareGithub,
+      },
+      {
+        name: "NPM",
+        Icon: DiNpm,
+      },
+      {
+        name: "Yarn",
+        Icon: SiYarn,
+      },
+      {
+        name: "Pnpm",
+        Icon: SiPnpm,
+      },
+      {
+        name: "Netlify",
+        Icon: SiNetlify,
+      },
+      {
+        name: "Vercel",
+        Icon: SiVercel,
+      },
+      {
+        name: "Vite",
+        Icon: SiVite,
+      },
+    ],
+  },
+  {
+    name: "Tools",
+    stack: [
+      {
+        name: "VS Code",
+        Icon: DiVisualstudio,
+      },
+      {
+        name: "Postman",
+        Icon: SiPostman,
+      },
+      {
+        name: "Netbeans",
+        Icon: DiNetbeans,
+      },
+      {
+        name: "Eclipse",
+        Icon: DiEclipse,
+      },
+    ],
+  },
+  {
+    name: "Database",
+    stack: [
+      {
+        name: "MySQL",
+        Icon: DiMysql,
+      },
+      {
+        name: "SQLite",
+        Icon: SiSqlite,
+      },
+      {
+        name: "MariaDB",
+        Icon: SiMariadb,
+      },
+      {
+        name: "PostgreSQL",
+        Icon: SiPostgresql,
+      }
+    ],
+  },
+];
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,20 +1,29 @@
 import MainLayout from "../components/Layouts/MainLayout";
-import { DiHtml5 } from "react-icons/di";
+import { techStak } from "../data/data";
 
 const Skills = () => {
+
   return (
     <MainLayout title={"< Skills />"} pageName={"Skills"}>
-      <div className="text-slate-100 container grid grid-cols-1 md:grid-cols-2 gap-5">
-        <div className="">
-          <h3 className="font-bold fluid-lg uppercase text-center mb-5">
-            FrontEnd
-          </h3>
+      <div className="text-slate-100 container grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
 
-          <div className="w-20 h-20 bg-slate-950 rounded-full flex justify-center items-center flex-col">
-            <h5 className="sr-only md:not-sr-only fluid-md">HTML</h5>
-            <DiHtml5 className="w-10 h-10" />
+        {techStak.map(({ name, stack }, index) => (
+          <div className="border-2 border-yellow-600 p-5 rounded-xl" key={index}>
+            <h3 className="font-bold fluid-lg uppercase text-center mb-5">
+              {name}
+            </h3>
+
+            <div className="grid grid-cols-4 gap-4">
+              {stack.map(({ name, Icon }, index) => (
+                <div key={index} className="flex justify-center items-center flex-col gap-1">
+                  <h5 className="sr-only md:not-sr-only fluid-md">{name}</h5>
+                  <Icon className="w-10 h-10" />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        ))}
+
       </div>
     </MainLayout>
   );
