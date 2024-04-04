df1b9997fc15d7df687d822a64ce543aed2c6930
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-04-04 12:22:35 -0400

    Creando la Page para los proyectos

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -5,6 +5,7 @@ import { AnimatePresence } from "framer-motion";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
 import ContactPage from "./pages/ContactPage";
+import ProjectsPage from "./pages/ProjectsPage";
 
 const AppRoutes = () => {
   const location = useLocation();
@@ -15,7 +16,7 @@ const AppRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<div>Projects</div>} />
+        <Route path="/projects" element={<ProjectsPage /> }/>
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </AnimatePresence>
---
+++ src/pages/ProjectsPage.jsx
@@ -0,0 +1,14 @@
+import MainLayout from "../components/Layouts/MainLayout"
+
+
+const ProjectsPage = () => {
+  return (
+    <MainLayout title={"< My Projects />"} pageName={"Projects"}>
+      <main className="my-5">
+        
+      </main>
+    </MainLayout>
+  )
+}
+
+export default ProjectsPage
