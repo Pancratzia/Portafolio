bf978c4e4efd4048688e1b6c396470dd9267a166
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-04-04 12:46:46 -0400

    Comenzando con la creación de las CARDS de Projects

--- src/data/data.js
+++ src/data/data.js
@@ -289,3 +289,92 @@ export const techStak = [
     ],
   },
 ];
+
+export const PROJECTS = [
+  {
+    title: "Landing Page - Dra. Yuleima Pérez",
+    image: "landingpage",
+    description: "Landing Page para una ginecobstetra. Su propósito principal es mostrar las cuentas de Redes Sociales de la doctora, sus servicios y su contacto. El proyecto está alojado en Vercel y se encuentra correctamente indexado en Google Search.",
+    github: "https://github.com/ARLA-Dev/drayuleimaperez",
+    demo: "https://www.drayuleimaperez.com/",
+    tags: ["HTML", "CSS", "JavaScript", "AOS", "SwiperJS", "Vercel", "Google Search", "Landing Page", "SEO", "Google Search", "Leaflet"],
+  },
+  {
+    title: "Quiz App",
+    image: "quizapp",
+    description: "Una aplicación web para jugar un quiz de 10 preguntas sobre programación creada con React, Redux y Firebase. Permite jugar tantas veces como se desee y muestra un ranking con los diez mejores jugadores.",
+    github: "https://github.com/Pancratzia/Monthly-Projects/tree/main/quiz-app",
+    demo: "https://pancratzia-quizapp.netlify.app/",
+    tags: ["HTML", "CSS", "JavaScript", "React", "React Custom Hooks", "Redux", "Firebase", "Netlify", "Game", "Ranking"],
+  },
+  {
+    title: "Star Wars Wiki",
+    image: "starwarswiki",
+    description: "Una wiki de StarWars creada con React usando Custom Hooks, React Context y StarWars API. El proyecto también inlcuye un filtro para buscar personajes por nombre, y abre un Modal cuando un personaje es clickeado, que muestra más información sobre ellos.",
+    github: "https://github.com/Pancratzia/Monthly-Projects/tree/main/starwars-wiki",
+    demo: "https://pancratzia-starwarswiki.netlify.app/",
+    tags: ["HTML", "CSS", "JavaScript", "React", "SWAPI", "React Custom Hooks", "React Context", "Animated Background", "Recursivity"],
+  },
+  {
+    title: "Conecta 4",
+    image: "conecta4",
+    description: "Un juego de conecta 4 creado en React. Le permite jugar contra otro jugador y utiliza almacenamiento local para guardar los movimientos y puntajes.",
+    github: "https://github.com/Pancratzia/Monthly-Projects/tree/main/conecta-4",
+    demo: "https://pancratzia-conecta4.netlify.app/",
+    tags: ["HTML", "CSS", "JavaScript", "React", "Game"],
+  },
+  {
+    title: "Bienes Raices",
+    image: "bienesraices",
+    description: "Sitio Web para una agrencia de Real Estate para la venta de propiedades. Contiene un panel de administración para gestionar información relacionada con las propiedades, vendedores y publicaciones de blog.",
+    github: "https://github.com/Pancratzia/Web-Development-Repository/tree/main/06-BienesRaicesMVC",
+    demo: "https://pancratzia-bienesraices.000webhostapp.com/",
+    demoUser: "test@admin.com",
+    demoPass: "admin",
+    tags: ["HTML", "CSS", "JavaScript", "PHP", "MySQL", "MVC", "Admin"],
+  },
+];
+
+export const EXTRA_PROJECTS = [
+  {
+    title: "Portafolio Personal",
+    description: "Mi portafolio personal está hecho con React, TypeScript y SASS. Utiliza Vite para desarrollo rápido, la librería AOS para animaciones y SwiperJS para crear sliders. El proyecto está alojado en Netlify, y su código está disponible en GitHub. El proyecto integra FetchAPI y GitHub API para mostrar logros. Además, incluye soporte multilingüe.",
+    link: "https://github.com/Pancratzia/Portfolio",
+    tags: ["HTML", "JavaScript", "GitHub", "React", "TypeScript", "SASS", "AOS", "SwiperJS", "Netlify"],
+  },
+  {
+    title: "App Salon",
+    description: "Sitio Web para un salón de belleza donde una lista de servicios es mostrada para los clientes junto a la opción para agendar una cita. Incluye una sección de administración para gestionar la información de los clientes, las citas y los servicios.",
+    link: "https://github.com/Pancratzia/Web-Development-Repository/tree/main/07-AppSalon",
+    tags: ["HTML", "CSS", "JavaScript", "PHP", "MySQL", "MVC", "PHPMailer", "Admin"],
+  },
+  {
+    title: "UpTask",
+    description: "Una aplicación de tareas simple donde cada usuario puede registrar y crear proyectos, que está formado por tareas que se pueden borrar, modificar y marcar como completadas.",
+    link: "https://github.com/Pancratzia/Web-Development-Repository/tree/main/08-UpTask",
+    tags: ["HTML", "CSS", "JavaScript", "PHP", "MySQL", "MVC", "PHPMailer"],
+  },
+  {
+    title: "P.P.P",
+    description: "Este proyecto ayuda a la empresa 'Pérez Productos Clean' a gestionar el inventario, los precios, y los ingredientes de fabricación. Desarrollada con Java 8, JasperReports, y MySQL.",
+    link: "https://github.com/Pancratzia/Web-Development-Repository/tree/main/09-PankraCamp",
+    tags: ["HTML", "CSS", "JavaScript", "PHP", "MySQL", "MVC", "PHPMailer", "Admin", "PayPal"],
+  },
+  {
+    title: "Blog en React y NodeJS",
+    description: "Un blog en React creado con NodeJS, React, y PostgreSQL. Este proyecto permite a los usuarios escribir, modificar y borrar artículos de blog con diferentes categorías. Librerías como React Router Dom, React Quill y Multer son utilizadas.",
+    link: "https://github.com/Pancratzia/React-And-NodeJs-Blog",
+    tags: ["React", "NodeJS", "PostgreSQL", "React Quill", "React Router Dom", "Multer", "SASS"],
+  },
+  {
+    title: "Femisys",
+    description: "Creada por ArLa junto a Arthuro Dugarte (ArtDugarte en GitHub). Desarrollada con HTML, Bootstrap, SpringBoot, Electron, y PostgreSQL. Este sistema fue diseño para un doctor que necesita una solución más eficiente que la existente.",
+    link: "https://github.com/Arla-Inc/FemiSys",
+    tags: ["HTML", "Bootstrap", "SpringBoot", "Electron", "PostgreSQL"],
+  },
+  {
+    title: "PankraCamp",
+    description: "Proyecto para un evento de dos días para conferencias y talleres. Incluye un área administrativa para manejar la información de los eventos y un sistema de registro de usuarios para los participantes. Los participantes pueden comprar tres tipos de entradas y hacer pagos con PayPal.",
+    tags: ["Java", "JasperReports", "MySQL", "Java Swing"],
+  },
+];
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,11 +1,50 @@
+import { Link } from "react-router-dom"
 import MainLayout from "../components/Layouts/MainLayout"
-
+import { PROJECTS } from "../data/data"
+import { FaGithub, FaEye } from "react-icons/fa6"
 
 const ProjectsPage = () => {
   return (
     <MainLayout title={"< My Projects />"} pageName={"Projects"}>
       <main className="my-5">
-        
+        <div className="container">
+            <h3 className="font-bold fluid-2xl uppercase text-center mb-5 italic   text-yellow-600">My Projects</h3>
+
+            <div className="mt-2 border-yellow-600 border rounded-xl p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+
+              {PROJECTS.map((project, index) => (
+                <div key={index} className="mb-5 text-slate-300">
+                  <h3 className="font-bold fluid-lg uppercase text-center mb-5">{project.title}</h3>
+
+                  <div className="grid grid-cols-2 gap-5">
+                    <div>
+
+                      <img src={`./img/projects/${project.image}.png`} alt={project.title} className="rounded-xl" />
+
+                    </div>
+
+                    <div>
+
+                      <p className="text-justify">{project.description}</p>
+
+                      <div className="grid grid-cols-2 gap-5">
+                        <Link to={project.github} target="_blank" rel="noreferrer">
+                          <FaGithub className="w-8 h-8 md:w-10 md:h-10 text-slate-300" />
+                        </Link>
+
+                        <Link to={project.demo} target="_blank" rel="noreferrer">
+                          <FaEye className="w-8 h-8 md:w-10 md:h-10 text-slate-300" />
+                        </Link>
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </div>
+              ))}
+            </div>
+        </div>
       </main>
     </MainLayout>
   )
