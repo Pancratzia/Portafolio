53f65c32262a142f2d81231a012414cd82d810d5
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-04-05 17:20:46 -0400

    Creando el componente Project para las cards de los proyectos individuales

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom"
 import MainLayout from "../components/Layouts/MainLayout"
 import { PROJECTS } from "../data/data"
-import { FaGithub, FaEye } from "react-icons/fa6"
+import Project from "../components/Projects/Project"
 
 const ProjectsPage = () => {
   return (
@@ -14,37 +13,7 @@ const ProjectsPage = () => {
 
             
               {PROJECTS.map((project, index) => (
-                <div key={index} className="mb-5 text-slate-300  border-y-2 p-5">
-                  <h3 className="font-bold fluid-lg uppercase text-center mb-5 text-yellow-500">{project.title}</h3>
-
-                  <div className="grid grid-cols-[1fr_2fr] gap-5">
-                    <div className="grid content-center">
-
-                      <img src={`./img/projects/${project.image}.png`} alt={project.title} className="rounded-xl" />
-
-                    </div>
-
-                    <div>
-
-                      <p className="text-center">{project.description}</p>
-
-                      <div className="grid grid-cols-2 gap-5 mt-5">
-                        <Link to={project.github} target="_blank" rel="noreferrer" className="flex justify-center items-center flex-col gap-1">
-                          <FaGithub className="w-5 h-5 md:w-8 md:h-8 text-slate-300" />
-                          <span className="sr-only lg:not-sr-only fluid-sm">Github</span>
-                        </Link>
-
-                        <Link to={project.demo} target="_blank" rel="noreferrer" className="flex justify-center items-center flex-col gap-1">
-                          <FaEye className="w-5 h-5 md:w-8 md:h-8 text-slate-300" />
-                          <span className="sr-only lg:not-sr-only fluid-sm">Demo</span>
-                        </Link>
-                      </div>
-
-                    </div>
-
-                  </div>
-
-                </div>
+                <Project key={index} project={project} />
               ))}
             </div>
         </div>
