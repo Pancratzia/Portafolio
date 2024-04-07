88d42bfc6d2f3bbf4f5d77584e90364ac5bcad3a
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-04-07 16:43:33 -0400

    Se comienza a a implementar el swiper para los extra projects

--- src/components/Projects/YellowCard.jsx
+++ src/components/Projects/YellowCard.jsx
@@ -1,8 +1,11 @@
 import PropTypes from "prop-types";
 
-const YellowCard = ({ children }) => {
+const YellowCard = ({ children, grid = true }) => {
+  const styles = `${`mt-2 border-yellow-600 border rounded-xl p-5`} ${ grid ? `grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8` : ""}`
+
+  
   return (
-    <div className="mt-2 border-yellow-600 border rounded-xl p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <div className={styles}>
       {children}
     </div>
   );
@@ -11,5 +14,6 @@ const YellowCard = ({ children }) => {
 export default YellowCard;
 
 YellowCard.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  grid: PropTypes.bool
 }
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,30 +1,53 @@
-import MainLayout from "../components/Layouts/MainLayout"
-import { PROJECTS } from "../data/data"
-import Project from "../components/Projects/Project"
-import YellowCard from "../components/Projects/YellowCard"
+import MainLayout from "../components/Layouts/MainLayout";
+import { PROJECTS } from "../data/data";
+import Project from "../components/Projects/Project";
+import YellowCard from "../components/Projects/YellowCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "./../components/Projects/swiperOverride.css";
 
 const ProjectsPage = () => {
   return (
     <MainLayout title={"< My Projects />"} pageName={"Projects"}>
       <main className="my-5">
         <div className="container">
-            <h3 className="font-bold fluid-2xl uppercase text-center mb-5 italic   text-yellow-600">My Projects</h3>
+          <h3 className="font-bold fluid-2xl uppercase text-center mb-5 italic   text-yellow-600">
+            My Projects
+          </h3>
 
-            <YellowCard>
+          <YellowCard>
             {PROJECTS.map((project, index) => (
-                <Project key={index} project={project} />
-              ))}
-            </YellowCard>
+              <Project key={index} project={project} />
+            ))}
+          </YellowCard>
 
-            <h3 className="font-bold fluid-2xl uppercase text-center my-5 italic   text-yellow-600">Extra Projects</h3>
+          <h3 className="font-bold fluid-2xl uppercase text-center my-5 italic   text-yellow-600">
+            Extra Projects
+          </h3>
 
-            <YellowCard>
-              
-            </YellowCard>
+          <YellowCard grid={false}>
+            <Swiper
+              navigation={true}
+              modules={[Navigation]}
+              className="text-slate-300 container"
+            >
+              <SwiperSlide>Slide 1</SwiperSlide>
+              <SwiperSlide>Slide 2</SwiperSlide>
+              <SwiperSlide>Slide 3</SwiperSlide>
+              <SwiperSlide>Slide 4</SwiperSlide>
+              <SwiperSlide>Slide 5</SwiperSlide>
+              <SwiperSlide>Slide 6</SwiperSlide>
+              <SwiperSlide>Slide 7</SwiperSlide>
+              <SwiperSlide>Slide 8</SwiperSlide>
+              <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
+          </YellowCard>
         </div>
       </main>
     </MainLayout>
-  )
-}
+  );
+};
 
 export default ProjectsPage;
