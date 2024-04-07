bef0c588b8f993351ae266c886926be71edaed63
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-04-07 17:05:31 -0400

    Se muestran los extra projects y se usa auto-play en los mismos

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -2,48 +2,58 @@ import { FaEye, FaGithub } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const Project = ({ project }) => {
-
-    const { title, image, description, github, demo } = project;
+const Project = ({ project, isPrincipal = true }) => {
+  const { title, image, description, github, demo, link } = project;
   return (
     <div className="mb-5 text-slate-300  border-y-2 p-5">
       <h3 className="font-bold fluid-lg uppercase text-center mb-5 text-yellow-500">
         {title}
       </h3>
 
-      <div className="grid grid-cols-[1fr_2fr] gap-5">
-        <div className="grid content-center">
-          <img
-            src={`./img/projects/${image}.png`}
-            alt={title}
-            className="rounded-xl"
-          />
-        </div>
+      <div className={isPrincipal ? "grid grid-cols-[1fr_2fr] gap-5" : "grid gap-5"}>
+        {isPrincipal && (
+          <div className="grid content-center">
+            <img
+              src={`./img/projects/${image}.png`}
+              alt={title}
+              className="rounded-xl"
+            />
+          </div>
+        )}
 
         <div>
           <p className="text-center">{description}</p>
 
-          <div className="grid grid-cols-2 gap-5 mt-5">
-            <Link
-              to={github}
-              target="_blank"
-              rel="noreferrer"
-              className="flex justify-center items-center flex-col gap-1"
-            >
-              <FaGithub className="w-5 h-5 md:w-8 md:h-8 text-slate-300" />
-              <span className="sr-only lg:not-sr-only fluid-sm">Github</span>
-            </Link>
+          {isPrincipal && (
+            <div className="grid grid-cols-2 gap-5 mt-5">
+              <Link
+                to={github}
+                target="_blank"
+                rel="noreferrer"
+                className="flex justify-center items-center flex-col gap-1"
+              >
+                <FaGithub className="w-5 h-5 md:w-8 md:h-8 text-slate-300" />
+                <span className="sr-only lg:not-sr-only fluid-sm">Github</span>
+              </Link>
 
-            <Link
-              to={demo}
-              target="_blank"
-              rel="noreferrer"
-              className="flex justify-center items-center flex-col gap-1"
-            >
-              <FaEye className="w-5 h-5 md:w-8 md:h-8 text-slate-300" />
-              <span className="sr-only lg:not-sr-only fluid-sm">Demo</span>
+              <Link
+                to={demo}
+                target="_blank"
+                rel="noreferrer"
+                className="flex justify-center items-center flex-col gap-1"
+              >
+                <FaEye className="w-5 h-5 md:w-8 md:h-8 text-slate-300" />
+                <span className="sr-only lg:not-sr-only fluid-sm">Demo</span>
+              </Link>
+            </div>
+          )}
+
+          {!isPrincipal && (
+            <Link to={link} target="_blank" rel="noreferrer" className="flex justify-center items-center flex-col gap-1 mt-5">
+              <FaGithub className="w-5 h-5 md:w-8 md:h-8 text-slate-300" />
+                <span className="sr-only lg:not-sr-only fluid-sm">Github</span>
             </Link>
-          </div>
+          )}
         </div>
       </div>
     </div>
@@ -54,4 +64,5 @@ export default Project;
 
 Project.propTypes = {
   project: PropTypes.object.isRequired,
-}
+  isPrincipal: PropTypes.bool,
+};
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,12 +1,11 @@
 import MainLayout from "../components/Layouts/MainLayout";
-import { PROJECTS } from "../data/data";
+import { PROJECTS, EXTRA_PROJECTS } from "../data/data";
 import Project from "../components/Projects/Project";
 import YellowCard from "../components/Projects/YellowCard";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
 import "swiper/css";
-import "swiper/css/navigation";
 import "./../components/Projects/swiperOverride.css";
+import { Autoplay } from "swiper/modules";
 
 const ProjectsPage = () => {
   return (
@@ -23,25 +22,28 @@ const ProjectsPage = () => {
             ))}
           </YellowCard>
 
-          <h3 className="font-bold fluid-2xl uppercase text-center my-5 italic   text-yellow-600">
+          <h3 className="font-bold fluid-2xl uppercase text-center my-5 italic  text-yellow-600">
             Extra Projects
           </h3>
 
           <YellowCard grid={false}>
             <Swiper
               navigation={true}
-              modules={[Navigation]}
-              className="text-slate-300 container"
+              centeredSlides={true}
+              grabCursor={true}
+              loop
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              modules={[Autoplay]}
+              className="text-slate-300 container bg-yell"
             >
-              <SwiperSlide>Slide 1</SwiperSlide>
-              <SwiperSlide>Slide 2</SwiperSlide>
-              <SwiperSlide>Slide 3</SwiperSlide>
-              <SwiperSlide>Slide 4</SwiperSlide>
-              <SwiperSlide>Slide 5</SwiperSlide>
-              <SwiperSlide>Slide 6</SwiperSlide>
-              <SwiperSlide>Slide 7</SwiperSlide>
-              <SwiperSlide>Slide 8</SwiperSlide>
-              <SwiperSlide>Slide 9</SwiperSlide>
+              {EXTRA_PROJECTS.map((project, index) => (
+                <SwiperSlide key={index}>
+                  <Project project={project} isPrincipal={false} />
+                </SwiperSlide>
+              ))}
             </Swiper>
           </YellowCard>
         </div>
