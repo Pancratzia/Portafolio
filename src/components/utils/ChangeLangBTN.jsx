622430a15674748faa964b6363096e35b1432257
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-04-15 20:11:52 -0400

    Se detecta cuando la página ha terminado de cargar para mostrar la traducción

--- src/components/utils/ChangeLangBTN.jsx
+++ src/components/utils/ChangeLangBTN.jsx
@@ -1,17 +1,47 @@
 import { useTranslation } from "react-i18next";
 import { IoLanguage } from "react-icons/io5";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const ChangeLangBTN = () => {
   const [t, i18n] = useTranslation("global");
+
+  const [isDocumentLoading, setIsDocumentLoading] = useState(true);
   const handleChangeLanguage = () => {
     i18n.changeLanguage(i18n.language === "en" ? "es" : "en");
   };
 
-  return <button className="text-slate-950 bg-indigo-100 border-indigo-100 border-2 p-2 flex justify-center items-center gap-2 fluid-md border-l-0 rounded-tr-md rounded-br-md
-   " onClick={handleChangeLanguage}>
-    <IoLanguage />
-    <span className="sr-only">{t("sr-only.changeLanguage")}</span>
-    </button>;
+  useEffect(() => {
+    const onPageLoad = () => {
+      setIsDocumentLoading(false);
+    };
+
+    if (document.readyState === "complete") {
+      onPageLoad();
+    } else {
+      window.addEventListener("load", onPageLoad, false);
+      return () => window.removeEventListener("load", onPageLoad);
+    }
+  }, []);
+
+  return (
+    <>
+      {isDocumentLoading || (
+        <motion.button
+          initial={{ opacity: 0, scale: 0, x: -100 }}
+          animate={{ opacity: 1, scale: 1, x: 0 }}
+          transition={{ duration: 3 }}
+          className="text-slate-950 bg-indigo-100 border-indigo-100 border-2 p-2 flex justify-center items-center gap-2 fluid-md border-l-0 rounded-tr-md rounded-br-md
+ "
+          onClick={handleChangeLanguage}
+        >
+          <IoLanguage />
+          <span className="sr-only">{t("sr-only.changeLanguage")}</span>
+        </motion.button>
+      )}
+    </>
+  );
 };
 
 export default ChangeLangBTN;
