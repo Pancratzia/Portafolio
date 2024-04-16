ac2a4852f81d5fc010b8203e7ec94005272c46a4
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-04-15 20:14:02 -0400

    Se acorta la transición

--- src/components/utils/ChangeLangBTN.jsx
+++ src/components/utils/ChangeLangBTN.jsx
@@ -31,7 +31,7 @@ const ChangeLangBTN = () => {
         <motion.button
           initial={{ opacity: 0, scale: 0, x: -100 }}
           animate={{ opacity: 1, scale: 1, x: 0 }}
-          transition={{ duration: 3 }}
+          transition={{ duration: 2 }}
           className="text-slate-950 bg-indigo-100 border-indigo-100 border-2 p-2 flex justify-center items-center gap-2 fluid-md border-l-0 rounded-tr-md rounded-br-md
  "
           onClick={handleChangeLanguage}
